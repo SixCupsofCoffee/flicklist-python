264100ca0de9d23e3d1d2d2f280a25381b3f27ed
John Bell <ja.bell@me.com>
2017-01-19 17:45:27 -0500

    Add movie list and today and tomorrow movie selection

--- main.py
+++ main.py
@@ -1,27 +1,36 @@
 import webapp2
+import random
 
 class Index(webapp2.RequestHandler):
 
     def getRandomMovie(self):
 
         # TODO: make a list with at least 5 movie titles
+        movieList = ["Star Trek", "Titanic", "Scott Pilgrim vs. The World", "The Lord of the Rings", "Star Wars", "The Big Lebowski", "The Matrix", "The Cabin in the Woods", "Indiana Jones", "Casablanca", "The Picture of Dorian Gray", "WarGames", "Hackers", "Back to the Future", "Ferris Bueller's Day Off", "Flash Gordon", "Ghostbusters", "Halloween", "Jurassic Park", "Paranormal Activity", "Shrek"]
 
         # TODO: randomly choose one of the movies, and return it
+        todaysMovie = movieList[random.randrange(0, len(movieList))]
 
-        return "The Big Lebowski"
+        return todaysMovie
 
     def get(self):
         # choose a movie by invoking our new function
         movie = self.getRandomMovie()
+        tomorrowsMovie = self.getRandomMovie()
 
         # build the response string
         content = "<h1>Movie of the Day</h1>"
         content += "<p>" + movie + "</p>"
 
-        # TODO: pick a different random movie, and display it under
-        # the heading "<h1>Tommorrow's Movie</h1>"
-
         self.response.write(content)
+
+        # TODO: pick a different random movie, and display it under
+        content2 = "<h1>Tomorrow's Movie</h1>"
+        content2 += "<p>" + tomorrowsMovie + "</p>"
+
+        self.response.write(content2)
+
+
 
 app = webapp2.WSGIApplication([
     ('/', Index)
